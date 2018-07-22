e20cc5cd05d23dbcfc8b1a897f4beffa590227f4
Johannes Hilden <hildenjohannes@gmail.com>
2018-07-22 12:38:36 +0300

    add server class

---
+++ src/server.cpp
@@ -0,0 +1,83 @@
+#include "server.h"
+
+using HttpServer = SimpleWeb::Server<SimpleWeb::HTTP>;
+
+namespace sagacious
+{
+    namespace http
+    {
+        request::request(std::shared_ptr<HttpServer::Request> request)
+          : _r(request)
+        {
+        }
+
+        response::response(std::shared_ptr<HttpServer::Response> response)
+          : _r(response),
+            _headers{}
+        {
+        }
+
+        void response::send(SimpleWeb::StatusCode code, std::string body)
+        {
+            _headers.emplace("Content-Length", std::to_string(body.length()));
+            _r->write(code, body, _headers);
+        }
+
+        void response::send_json(SimpleWeb::StatusCode code, std::string body)
+        {
+            _headers.emplace("Content-Type", "application/json");
+            send(code, body);
+        }
+
+        std::string to_string(const method &method)
+        {
+            switch (method)
+            {
+            case POST:
+                return "POST";
+            case PUT:
+                return "PUT";
+            case PATCH:
+                return "PATCH";
+            case DELETE:
+                return "DELETE";
+            default:
+                assert(method::GET == method);
+                return "GET";
+            }
+        }
+    }
+
+    server::server() : _server{}
+    {
+        set_port(9080);
+    }
+
+    server::~server()
+    {
+    }
+
+    void server::run()
+    {
+        _server.start();
+    }
+
+    void server::run(short port)
+    {
+        set_port(port);
+        run();
+    }
+
+    void server::on(
+        const http::method &method, 
+        std::string pattern, 
+        std::function<void(http::request req, http::response res)> handler)
+    {
+        _server.resource[pattern][http::to_string(method)] = [handler](
+            std::shared_ptr<HttpServer::Response> response, 
+            std::shared_ptr<HttpServer::Request> request) 
+        {
+            handler(http::request{request}, http::response{response});
+        };
+    }
+}
---
+++ src/server.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <cassert>
+#include <server_http.hpp>
+
+using HttpServer = SimpleWeb::Server<SimpleWeb::HTTP>;
+
+namespace sagacious
+{
+    namespace http
+    {
+        enum method { GET = 0, POST, PUT, PATCH, DELETE };
+
+        class request
+        {
+        public:
+            explicit request(std::shared_ptr<HttpServer::Request> request);
+
+        private:
+            std::shared_ptr<HttpServer::Request> _r;
+        };
+
+        class response
+        {
+        public:
+            explicit response(std::shared_ptr<HttpServer::Response> response);
+
+            void send(SimpleWeb::StatusCode code, std::string body);
+            void send_json(SimpleWeb::StatusCode code, std::string body);
+
+        private:
+            std::shared_ptr<HttpServer::Response> _r;
+            SimpleWeb::CaseInsensitiveMultimap    _headers;
+        };
+
+        std::string to_string(const method &method);
+    }
+
+    class server
+    {
+    public:
+        server();
+        ~server();
+
+        void run();
+        void run(short port);
+
+        inline void set_port(short port) { _server.config.port = port; }
+
+        void on(
+            const http::method &method, 
+            std::string pattern, 
+            std::function<void(http::request req, http::response res)> handler
+        );
+
+    private:
+        SimpleWeb::Server<SimpleWeb::HTTP> _server;
+    };
+}
